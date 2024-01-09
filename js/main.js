591a263b915969da34a388278ebbf2cbda783394
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-09 18:27:03 +0100

    generatedprice

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const generatedprice = document.getElementById(`generated-price`);
+
+// Chiediamo all'utente quanti chilometri vuole percorrere
+const km = parseInt(prompt(`Quanti chilometri vuoi percorrere?`));
+console.log(km);
+
+
+// Chiediamo l'età all'utente
+const age = parseInt(prompt(`Quanti anni hai?`));
+console.log(age);
+
+// Calcoliamo il prezzo del biglietto in base ai chilometri che l'utente vuole percorrere
+let price = km * 0.21;
+console.log(price);
+let finalprice = price;
+
+if(age < 18){
+    let discountprice = 20;
+
+    discountprice = price * discountprice / 100;
+    finalprice = price - discountprice;
+}
+else if (age > 65){
+    let discountprice = 40;
+
+    discountprice = price * discountprice / 100;
+    finalprice = price - discountprice;
+}
+console.log(finalprice);
+generatedprice.innerText = `Il prezzo del tuo biglietto è: ` + finalprice.toFixed(2) + ` €`;
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Traccia
+
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65
+
+## Soluzione
+1. Chiediamo all'utente quanti chilometri vuole percorrere
+2. Chiediamo l'età all'utente
+ 3. Calcoliamo il prezzo del biglietto in base ai chilometri che l'utente vuole percorrere:
+- SE l'utente è minorenne applichiamo lo sconto del 20%
+- ALTRIMENTI SE l'utente ha più di 65 anni applichiamo uno sconto del 40%
+
+4. Stampiamo il prezzo del biglietto all'utente.
